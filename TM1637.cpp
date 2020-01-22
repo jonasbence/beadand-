78be0372fa6a91194b17b8fd290598f546303108
duncsi <56126326+duncsi@users.noreply.github.com>
2020-01-22 22:00:55 +0100

    Hőmérő vez

--- TM1637.cpp
+++ TM1637.cpp
@@ -1,53 +1,148 @@
+
 #include "TM1637.h"
-
-#include <DHT.h> 
-#define DHTPIN A3
-#define DHTTYPE DHT11   
-
-DHT dht(DHTPIN, DHTTYPE); 
- 
-#define CLK 4     
-#define DIO 5
- 
-TM1637 tm1637(CLK,DIO);
-
- 
-int fenyerzekelo = A2;
-int motor = 6;
-int gomb = A5;
-int nedvesseg = A1;
-void setup(){
-  pinMode(gomb,INPUT_PULLUP);
-  pinMode(fenyerzekelo,INPUT);
-  pinMode(nedvesseg,INPUT);
-  pinMode(motor,OUTPUT);
-  
-  tm1637.init();
-  tm1637.set(BRIGHT_DARKEST); 
- 
-  delay(1500);
-  dht.begin();   
-  
+#include <Arduino.h>
+static int8_t TubeTab[] = {0x3f,0x06,0x5b,0x4f,
+                           0x66,0x6d,0x7d,0x07,
+                           0x7f,0x6f,0x77,0x7c,
+                           0x39,0x5e,0x79,0x71};//0~9,A,b,C,d,E,F                        
+TM1637::TM1637(uint8_t Clk, uint8_t Data)
+{
+  Clkpin = Clk;
+  Datapin = Data;
+  pinMode(Clkpin,OUTPUT);
+  pinMode(Datapin,OUTPUT);
 }
 
-void loop(){
+void TM1637::init(void)
+{
+  clearDisplay();
+}
+
+void TM1637::writeByte(int8_t wr_data)
+{
+  uint8_t i,count1;   
+  for(i=0;i<8;i++)        //sent 8bit data
+  {
+    digitalWrite(Clkpin,LOW);      
+    if(wr_data & 0x01)digitalWrite(Datapin,HIGH);//LSB first
+    else digitalWrite(Datapin,LOW);
+    wr_data >>= 1;      
+    digitalWrite(Clkpin,HIGH);
+      
+  }  
+  digitalWrite(Clkpin,LOW); //wait for the ACK
+  digitalWrite(Datapin,HIGH);
+  digitalWrite(Clkpin,HIGH);     
+  pinMode(Datapin,INPUT);
+  while(digitalRead(Datapin))    
+  { 
+    count1 +=1;
+    if(count1 == 200)//
+    {
+     pinMode(Datapin,OUTPUT);
+     digitalWrite(Datapin,LOW);
+     count1 =0;
+    }
+    pinMode(Datapin,INPUT);
+  }
+  pinMode(Datapin,OUTPUT);
   
-   int temp = dht.readTemperature();  
- 
- int digitoneT = temp / 10;
- int digittwoT = temp % 10;
+}
+//send start signal to TM1637
+void TM1637::start(void)
+{
+  digitalWrite(Clkpin,HIGH);//send start signal to TM1637
+  digitalWrite(Datapin,HIGH); 
+  digitalWrite(Datapin,LOW); 
+  digitalWrite(Clkpin,LOW); 
+} 
+//End of transmission
+void TM1637::stop(void)
+{
+  digitalWrite(Clkpin,LOW);
+  digitalWrite(Datapin,LOW);
+  digitalWrite(Clkpin,HIGH);
+  digitalWrite(Datapin,HIGH); 
+}
+//display function.Write to full-screen.
+void TM1637::display(int8_t DispData[])
+{
+  int8_t SegData[4];
+  uint8_t i;
+  for(i = 0;i < 4;i ++)
+  {
+    SegData[i] = DispData[i];
+  }
+  coding(SegData);
+  start();          //start signal sent to TM1637 from MCU
+  writeByte(ADDR_AUTO);//
+  stop();           //
+  start();          //
+  writeByte(Cmd_SetAddr);//
+  for(i=0;i < 4;i ++)
+  {
+    writeByte(SegData[i]);        //
+  }
+  stop();           //
+  start();          //
+  writeByte(Cmd_DispCtrl);//
+  stop();           //
+}
+//******************************************
+void TM1637::display(uint8_t BitAddr,int8_t DispData)
+{
+  int8_t SegData;
+  SegData = coding(DispData);
+  start();          //start signal sent to TM1637 from MCU
+  writeByte(ADDR_FIXED);//
+  stop();           //
+  start();          //
+  writeByte(BitAddr|0xc0);//
+  writeByte(SegData);//
+  stop();            //
+  start();          //
+  writeByte(Cmd_DispCtrl);//
+  stop();           //
+}
 
-    tm1637.display(0,digitoneT); 
-    tm1637.display(1,digittwoT);
-    tm1637.display(2,19); // fok
-    tm1637.display(3,12);  // C a végén
+void TM1637::clearDisplay(void)
+{
+  display(0x00,0x7f);
+  display(0x01,0x7f);
+  display(0x02,0x7f);
+  display(0x03,0x7f);  
+}
+//To take effect the next time it displays.
+void TM1637::set(uint8_t brightness,uint8_t SetData,uint8_t SetAddr)
+{
+  Cmd_SetData = SetData;
+  Cmd_SetAddr = SetAddr;
+  Cmd_DispCtrl = 0x88 + brightness;//Set the brightness and it takes effect the next time it displays.
+}
 
-// ha lenyomon a gombot vagy megfelelő hpmérséklet és megfelelő sötétség és megfelelő szárazság mellet locsól
- if((digitalRead(gomb)==HIGH) || (temp>15 && temp<25) && (analogRead(nedvesseg)<200) && (analogRead(fenyerzekelo)<200) ){
-  digitalWrite(motor, HIGH);
-  delay(2000);
- }else{
-  digitalWrite(motor, LOW);
-  delay(1000);
- }
+//Whether to light the clock point ":".
+//To take effect the next time it displays.
+void TM1637::point(boolean PointFlag)
+{
+  _PointFlag = PointFlag;
+}
+void TM1637::coding(int8_t DispData[])
+{
+  uint8_t PointData;
+  if(_PointFlag == POINT_ON)PointData = 0x80;
+  else PointData = 0; 
+  for(uint8_t i = 0;i < 4;i ++)
+  {
+    if(DispData[i] == 0x7f)DispData[i] = 0x00;
+    else DispData[i] = TubeTab[DispData[i]] + PointData;
+  }
+}
+int8_t TM1637::coding(int8_t DispData)
+{
+  uint8_t PointData;
+  if(_PointFlag == POINT_ON)PointData = 0x80;
+  else PointData = 0; 
+  if(DispData == 0x7f) DispData = 0x00 + PointData;//The bit digital tube off
+  else DispData = TubeTab[DispData] + PointData;
+  return DispData;
 }
